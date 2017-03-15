727d47a49f555e9a9cb7a9d29e0b399ce20fc34a
Lars Abbo <l.j.abbo@student.tudelft.nl>
2017-03-15 13:07:33 +0100

    Added basic MatchFormatter

--- src/formatter/Formatter.java
+++ src/formatter/Formatter.java
@@ -2,11 +2,29 @@ package formatter;
 
 import api.data.APIDataObject;
 
+/**
+ * The interface Formatter.
+ */
 public interface Formatter {
 
+	/**
+	 * Add an APIDataObject.
+	 *
+	 * @param obj the obj
+	 */
 	void add(APIDataObject obj);
 
-	void setFile(String file);
+	/**
+	 * Sets file the data is written to.
+	 *
+	 * @param fileName the file name
+	 */
+	void setFile(String fileName);
 
-	void setWriteAmount(int amount);
+	/**
+	 * Sets the amount of data the Formatter will store before it will write the data to the file.
+	 *
+	 * @param writeAmount the write amount
+	 */
+	void setWriteAmount(int writeAmount);
 }
--- src/formatter/MatchFormatter.java
+++ src/formatter/MatchFormatter.java
@@ -2,19 +2,77 @@ package formatter;
 
 import api.data.APIDataObject;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * The type Match formatter.
+ */
 public class MatchFormatter implements Formatter {
+
+	private String fileName;
+	private File file;
+	private FileWriter fileWriter;
+
+	private int writeAmount;
+
+	private String output;
+	private int outputCounter;
+
+	/**
+	 * Instantiates a new Match formatter.
+	 *
+	 * @param fileName the file name
+	 */
+	public MatchFormatter(String fileName) {
+		setFile(fileName);
+		file = new File(fileName);
+		try {
+			fileWriter = new FileWriter(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Instantiates a new Match formatter.
+	 *
+	 * @param fileName    the file name
+	 * @param writeAmount the write amount
+	 */
+	public MatchFormatter(String fileName, int writeAmount) {
+		this(fileName);
+		setWriteAmount(writeAmount);
+	}
+
 	@Override
 	public void add(APIDataObject obj) {
+		output += formatAPIDataObject(obj);
+		outputCounter++;
 
+		if (outputCounter > writeAmount) {
+			try {
+				fileWriter.append(output);
+				output = "";
+				outputCounter = 0;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private String formatAPIDataObject(APIDataObject obj) {
+		return null;
 	}
 
 	@Override
-	public void setFile(String file) {
-
+	public void setFile(String fileName) {
+		this.fileName = fileName;
 	}
 
 	@Override
-	public void setWriteAmount(int amount) {
-
+	public void setWriteAmount(int writeAmount) {
+		this.writeAmount = writeAmount;
 	}
 }
